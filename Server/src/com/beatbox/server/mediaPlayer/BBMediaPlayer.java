bc66eef693d01623b23faf40a3fc775c2655bc51
Shawn Busolits <shawn.busolits@gmail.com>
2013-01-26 13:07:56 -0500

    moved playbac implementation to library

--- Server/src/com/beatbox/server/mediaPlayer/BBMediaPlayer.java
+++ Server/src/com/beatbox/server/mediaPlayer/BBMediaPlayer.java
@@ -1,17 +1,7 @@
 package com.beatbox.server.mediaPlayer;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-
-import javax.media.Format;
-import javax.media.Manager;
-import javax.media.MediaLocator;
-import javax.media.NoPlayerException;
 import javax.media.Player;
-import javax.media.PlugInManager;
 import javax.media.Time;
-import javax.media.format.AudioFormat;
 
 import com.beatbox.lib.song.Song;
 import com.beatbox.server.ServerController;
@@ -21,6 +11,7 @@ public class BBMediaPlayer extends Thread {
 	Player player;
 	Time resumeTime;
 	boolean paused;
+	Song currentSong;
 
 	public BBMediaPlayer(ServerController controller) {
 		this.controller = controller;
@@ -31,46 +22,17 @@ public class BBMediaPlayer extends Thread {
 	}
 
 	public void playNextSong() {
-		Song nextSong = controller.getNextSong();
-		File songFile = new File(nextSong.getPath());
-		MediaLocator ml;
-		Format input1 = new AudioFormat(AudioFormat.MPEGLAYER3);
-		Format input2 = new AudioFormat(AudioFormat.MPEG);
-		Format output = new AudioFormat(AudioFormat.LINEAR);
-		PlugInManager.addPlugIn("com.sun.media.codec.audio.mp3.JavaDecoder",
-				new Format[] { input1, input2 }, new Format[] { output },
-				PlugInManager.CODEC);
-		try {
-			ml = new MediaLocator(songFile.toURI().toURL());
-			player = Manager.createPlayer(ml);
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoPlayerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		player.start();
+		currentSong = controller.getNextSong();
+		currentSong.play();
 		controller.setPlaying(true);
-		paused = false;
 	}
 
 	public void stopMedia() {
-		player.stop();
+		currentSong.stop();
 		controller.setPlaying(false);
 	}
 
 	public void pauseAndResumeMedia() {
-		if (paused) {
-			player.setMediaTime(resumeTime);
-			player.start();
-		} else {
-			resumeTime = player.getMediaTime();
-			player.stop();
-		}
-		paused = !paused;
+		currentSong.pauseOrResume();
 	}
 }
